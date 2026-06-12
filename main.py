8c98fb818c835addcb299a4481c300a7a806e5cb
dackzEspana <dackz07@gmail.com>
2026-06-12 14:28:17 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-from flask import Flask
+from flask import Flask, request, jsonify, send_file
+import requests
+import pandas as pd
+from ta.momentum import RSIIndicator
+from PIL import Image, ImageDraw, ImageFont
+import mplfinance as mpf
+import matplotlib.pyplot as plt
+import os
+from datetime import datetime, timedelta
+
+
+#====WEBHOOK====
 
 app = Flask(__name__)
 
@@ -6,5 +17,20 @@ app = Flask(__name__)
 def home():
     return "SwingBot Online"
 
+@app.route("/webhook", methods=["POST"])
+def webhook():
+
+    data = request.json
+
+    print("ALERTA RECIBIDA:")
+    print(data)
+
+    return jsonify({
+        "status": "ok",
+        "message": "alerta recibida"
+    })
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
+
+#====FIN WEBHOOK====
