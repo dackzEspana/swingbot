3286548e0d7f109bc386baedd6375e7da40fb21f
dackzEspana <dackz07@gmail.com>
2026-06-22 15:54:50 -0600

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,71 @@ DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1515092627865731072/w2UX
 
 #==== FIN CONEXIONES ====
 
-#==== GRAFICO ===
+#==== LIMPIAR SYMBOL ====
 
-def generar_grafico(df, symbol):
+def limpiar_symbol(symbol):
+
+    symbol = symbol.replace(".P", "")
+    symbol = symbol.replace("USDT", "-USDT")
+
+    return symbol
+
+#==== FIN LIMPIAR SYMBOL ====
+
+
+#==== OBTENER VELAS ====
+
+def obtener_velas(symbol):
+
+    url = "https://www.okx.com/api/v5/market/candles"
+
+    params = {
+        "instId": symbol,
+        "bar": "15m",
+        "limit": "50"
+    }
+
+    response = requests.get(url, params=params)
+
+    data = response.json()["data"]
+
+    df = pd.DataFrame(
+        data,
+        columns=[
+            "timestamp",
+            "open",
+            "high",
+            "low",
+            "close",
+            "volume",
+            "volCcy",
+            "volCcyQuote",
+            "confirm"
+        ]
+    )
+
+    df = df.iloc[::-1]
+
+    df["timestamp"] = pd.to_datetime(
+        df["timestamp"].astype(float),
+        unit="ms"
+    )
+
+    df.set_index("timestamp", inplace=True)
+
+    df["open"] = df["open"].astype(float)
+    df["high"] = df["high"].astype(float)
+    df["low"] = df["low"].astype(float)
+    df["close"] = df["close"].astype(float)
+
+    return df
+
+#==== FIN OBTENER VELAS ====
+
+
+#==== GENERAR GRAFICO ====
+
+def generar_grafico(df):
 
     mc = mpf.make_marketcolors(
         up='#7CFC00',
@@ -47,69 +109,97 @@ def generar_grafico(df, symbol):
         )
     )
 
-    return "grafico.png"
-
-#==== FIN GRAFICO ====
-
-#==== GRAFICO EN IMAGEN ====
-
-plantilla = Image.open("Señal_Venta.png")
-
-grafico = Image.open("grafico.png")
-
-grafico = grafico.resize((1320, 520))
-
-plantilla.paste(
-    grafico,
-    (30, 520)
-)
-
-plantilla.save("resultado.png")
-
-#==== FINGRAFICO EN IMAGEN ====
+#==== FIN GENERAR GRAFICO ====
 
 
-#====WEBHOOK====
+#==== CREAR IMAGEN ====
+
+def crear_imagen(signal, symbol, price):
+
+    if signal == "BUY":
+        plantilla = Image.open("buy.png")
+
+    else:
+        plantilla = Image.open("sell.png")
+
+    grafico = Image.open("grafico.png")
+
+    # AJUSTAREMOS ESTAS MEDIDAS DESPUES
+    grafico = grafico.resize((1320, 520))
+
+    plantilla.paste(
+        grafico,
+        (30, 500)
+    )
+
+    plantilla.save("senal_final.png")
+
+    return "senal_final.png"
+
+#==== FIN CREAR IMAGEN ====
+
+
+#==== ENVIAR DISCORD ====
+
+def enviar_discord():
+
+    with open("senal_final.png", "rb") as file:
+
+        requests.post(
+            DISCORD_WEBHOOK_URL,
+            files={
+                "file": file
+            }
+        )
+
+#==== FIN ENVIAR DISCORD ====
+
+
+#==== WEBHOOK ====
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "SwingBot Online"
 
+
 @app.route("/webhook", methods=["POST"])
 def webhook():
 
     data = request.json
 
-    signal = data.get("signal", "N/A")
-    symbol = data.get("symbol", "N/A")
-    price = data.get("price", "N/A")
+    signal = data["signal"]
+    symbol_tv = data["symbol"]
+    price = data["price"]
 
-    mensaje = f"""
-🚀 ABC SWING BOT
+    symbol_okx = limpiar_symbol(symbol_tv)
 
-📈 Señal: {signal}
-🪙 Par: {symbol}
-💰 Precio: {price}
-"""
+    df = obtener_velas(symbol_okx)
 
-    requests.post(
-        DISCORD_WEBHOOK_URL,
-        json={"content": mensaje}
+    generar_grafico(df)
+
+    crear_imagen(
+        signal,
+        symbol_tv,
+        price
     )
 
-    print("========== ALERTA RECIBIDA ==========")
-    print(data)
-    print("=====================================")
+    enviar_discord()
 
     return jsonify({
-        "status": "ok",
-        "signal": signal,
-        "symbol": symbol,
-        "price": price
+        "status": "ok"
     })
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+#==== FIN WEBHOOK ====
 
-#====FIN WEBHOOK====
+
+#==== APP ====
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=8000
+    )
+
+#==== FIN APP ====
